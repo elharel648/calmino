fad5b5909630a148d7bac82b89c5d429bce37ce0
elharel648 <134229855+elharel648@users.noreply.github.com>
2026-03-27 10:55:07 +0300

    feat: re-enable CalmParentLiveActivity extension
    Re-registered ./plugins/withLiveActivity in app.json.
    appExtensions config was already present.
    All 9 Swift files confirmed via prebuild.

---
+++ ios/CalmParentLiveActivity/CalmParentLiveActivityBundle.swift
@@ -0,0 +1,14 @@
+import WidgetKit
+import SwiftUI
+
+@main
+struct CalmParentLiveActivityBundle: WidgetBundle {
+    var body: some Widget {
+        if #available(iOS 16.2, *) {
+            BabysitterShiftLiveActivity()
+            SleepLiveActivity()
+            FeedingLiveActivity()
+            BreastfeedingLiveActivity()
+        }
+    }
+}
